fcc5b3dfc4048754d0e9bd2724d2fcbd7feb2a1c
Faithtorian Goins <faithtoriangoins@gmail.com>
2023-02-02 15:20:12 -0600

    excersize 1 and 2

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace OperatorExercise
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace OperatorExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var a = 17;
+            var b = 4;
+           
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+
+            int qoutient = a / b;
+            int remainder = a % b;
+            if (a == 17 && b == 4);
+            Console.WriteLine($"{a}/{b} is {qoutient} remainder {remainder}.");
+            Console.WriteLine("what is the radius of yor circle?");
+            var radius = double.Parse(Console.ReadLine());
+            var answer = AreaOfCirle(radius);
+            Console.WriteLine($" the circle with radius {radius} is {answer}");
+        }
+        public static double AreaOfCirle(double radius) 
+        {
+            return (Math.PI * radius * radius);
         }
     }
+
 }
